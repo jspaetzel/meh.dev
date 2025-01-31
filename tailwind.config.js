ad4971fd45ceea5b13dd4e33bb90e0d7d0419ff6
John <john.spaetzel@gmail.com>
2025-01-30 23:10:21 -0800

    general 2025 update, style improvements, centering, better readability, icon updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,36 @@ module.exports = {
       './*.html',
     ],
     theme: {
-      theme: {
-        extend: {},
+      extend: {
+          typography: ({ theme }) => ({
+              DEFAULT: {
+                  css: {
+                      table: {
+                          width: '100%',
+                          borderCollapse: 'collapse',
+                      },
+                      'thead th': {
+                          backgroundColor: theme('colors.gray.100'),
+                          border: `1px solid ${theme('colors.gray.200')}`,
+                          padding: theme('spacing.2'),
+                          textAlign: 'left',
+                      },
+                      'tbody td': {
+                          border: `1px solid ${theme('colors.gray.200')}`,
+                          padding: theme('spacing.2'),
+                          '&:first-child': {
+                              paddingInlineStart: theme('spacing.4'),
+                          },
+                      },
+                      'tbody tr:nth-child(even)': {
+                          backgroundColor: theme('colors.gray.100'),
+                      },
+                  },
+              },
+          }),
       },
-    },
-    plugins: [
-        require('@tailwindcss/typography'),
-    ],
-  }
+  },
+  plugins: [
+      require('@tailwindcss/typography'),
+  ],
+}
